areek/elasticsearch

Handle transient write failure in transport replication action

core/src/main/java/org/elasticsearch/action/index/TransportIndexAction.java
@@ -39,11 +39,11 @@ import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.engine.Engine;
+import org.elasticsearch.index.mapper.MapperParsingException;
 import org.elasticsearch.index.mapper.Mapping;
 import org.elasticsearch.index.mapper.SourceToParse;
 import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.index.shard.ShardId;
-import org.elasticsearch.index.translog.Translog.Location;
 import org.elasticsearch.indices.IndexAlreadyExistsException;
 import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.tasks.Task;
@@ -140,48 +140,68 @@ public class TransportIndexAction extends TransportWriteAction<IndexRequest, Ind
     }
 
     @Override
-    protected WriteResult<IndexResponse> onPrimaryShard(IndexRequest request, IndexShard indexShard) throws Exception {
+    protected PrimaryOperationResult<IndexResponse> onPrimaryShard(IndexRequest request, IndexShard indexShard) throws Exception {
         return executeIndexRequestOnPrimary(request, indexShard, mappingUpdatedAction);
     }
 
     @Override
-    protected Location onReplicaShard(IndexRequest request, IndexShard indexShard) {
-        return executeIndexRequestOnReplica(request, indexShard).getTranslogLocation();
+    protected ReplicaOperationResult onReplicaShard(IndexRequest request, IndexShard indexShard) {
+        return executeIndexRequestOnReplica(request, indexShard);
     }
 
     /**
      * Execute the given {@link IndexRequest} on a replica shard, throwing a
      * {@link RetryOnReplicaException} if the operation needs to be re-tried.
      */
-    public static Engine.Index executeIndexRequestOnReplica(IndexRequest request, IndexShard indexShard) {
+    public static ReplicaOperationResult executeIndexRequestOnReplica(IndexRequest request, IndexShard indexShard) {
         final ShardId shardId = indexShard.shardId();
         SourceToParse sourceToParse = SourceToParse.source(SourceToParse.Origin.REPLICA, shardId.getIndexName(), request.type(), request.id(), request.source())
                 .routing(request.routing()).parent(request.parent()).timestamp(request.timestamp()).ttl(request.ttl());
 
-        final Engine.Index operation = indexShard.prepareIndexOnReplica(sourceToParse, request.version(), request.versionType(), request.getAutoGeneratedTimestamp(), request.isRetry());
+        final Engine.Index operation;
+        try {
+            operation = indexShard.prepareIndexOnReplica(sourceToParse, request.version(), request.versionType(), request.getAutoGeneratedTimestamp(), request.isRetry());
+        } catch (MapperParsingException | IllegalArgumentException e) {
+            return new ReplicaOperationResult(e);
+        }
         Mapping update = operation.parsedDoc().dynamicMappingsUpdate();
         if (update != null) {
             throw new RetryOnReplicaException(shardId, "Mappings are not available on the replica yet, triggered update: " + update);
         }
         indexShard.index(operation);
-        return operation;
+        if (operation.hasFailure()) {
+            return new ReplicaOperationResult(operation.getFailure());
+        } else {
+            return new ReplicaOperationResult(operation.getTranslogLocation());
+        }
     }
 
     /** Utility method to prepare an index operation on primary shards */
-    public static Engine.Index prepareIndexOperationOnPrimary(IndexRequest request, IndexShard indexShard) {
+    static Engine.Index prepareIndexOperationOnPrimary(IndexRequest request, IndexShard indexShard) {
         SourceToParse sourceToParse = SourceToParse.source(SourceToParse.Origin.PRIMARY, request.index(), request.type(), request.id(), request.source())
             .routing(request.routing()).parent(request.parent()).timestamp(request.timestamp()).ttl(request.ttl());
         return indexShard.prepareIndexOnPrimary(sourceToParse, request.version(), request.versionType(), request.getAutoGeneratedTimestamp(), request.isRetry());
     }
 
-    public static WriteResult<IndexResponse> executeIndexRequestOnPrimary(IndexRequest request, IndexShard indexShard,
+    public static PrimaryOperationResult<IndexResponse> executeIndexRequestOnPrimary(IndexRequest request, IndexShard indexShard,
             MappingUpdatedAction mappingUpdatedAction) throws Exception {
-        Engine.Index operation = prepareIndexOperationOnPrimary(request, indexShard);
+        Engine.Index operation;
+        try {
+            operation = prepareIndexOperationOnPrimary(request, indexShard);
+        } catch (MapperParsingException | IllegalArgumentException e) {
+            return new PrimaryOperationResult<>(e);
+        }
         Mapping update = operation.parsedDoc().dynamicMappingsUpdate();
         final ShardId shardId = indexShard.shardId();
         if (update != null) {
-            mappingUpdatedAction.updateMappingOnMaster(shardId.getIndex(), request.type(), update);
-            operation = prepareIndexOperationOnPrimary(request, indexShard);
+            try {
+                // can throw timeout exception when updating mappings or ISE for attempting to update default mappings
+                // which are bubbled up
+                mappingUpdatedAction.updateMappingOnMaster(shardId.getIndex(), request.type(), update);
+                operation = prepareIndexOperationOnPrimary(request, indexShard);
+            } catch (MapperParsingException | IllegalArgumentException e) {
+                return new PrimaryOperationResult<>(e);
+            }
             update = operation.parsedDoc().dynamicMappingsUpdate();
             if (update != null) {
                 throw new ReplicationOperation.RetryOnPrimaryException(shardId,
@@ -189,16 +209,19 @@ public class TransportIndexAction extends TransportWriteAction<IndexRequest, Ind
             }
         }
         indexShard.index(operation);
+        if (operation.hasFailure()) {
+            return new PrimaryOperationResult<>(operation.getFailure());
+        } else {
+            // update the version on request so it will happen on the replicas
+            final long version = operation.version();
+            request.version(version);
+            request.versionType(request.versionType().versionTypeForReplicationAndRecovery());
 
-        // update the version on request so it will happen on the replicas
-        final long version = operation.version();
-        request.version(version);
-        request.versionType(request.versionType().versionTypeForReplicationAndRecovery());
+            assert request.versionType().validateVersionForWrites(request.version());
 
-        assert request.versionType().validateVersionForWrites(request.version());
-
-        IndexResponse response = new IndexResponse(shardId, request.type(), request.id(), request.version(), operation.isCreated());
-        return new WriteResult<>(response, operation.getTranslogLocation());
+            IndexResponse response = new IndexResponse(shardId, request.type(), request.id(), request.version(), operation.isCreated());
+            return new PrimaryOperationResult<>(response, operation.getTranslogLocation());
+        }
     }
 }
 

core/src/test/java/org/elasticsearch/action/support/replication/TransportWriteActionTests.java
@@ -28,7 +28,6 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.index.translog.Translog;
-import org.elasticsearch.index.translog.Translog.Location;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
@@ -137,13 +136,13 @@ public class TransportWriteActionTests extends ESTestCase {
         }
 
         @Override
-        protected WriteResult<TestResponse> onPrimaryShard(TestRequest request, IndexShard indexShard) throws Exception {
-            return new WriteResult<>(new TestResponse(), location);
+        protected PrimaryOperationResult<TestResponse> onPrimaryShard(TestRequest request, IndexShard indexShard) throws Exception {
+            return new PrimaryOperationResult<>(new TestResponse(), location);
         }
 
         @Override
-        protected Location onReplicaShard(TestRequest request, IndexShard indexShard) {
-            return location;
+        protected ReplicaOperationResult onReplicaShard(TestRequest request, IndexShard indexShard) {
+            return new ReplicaOperationResult(location);
         }
 
         @Override

ochprince/OpenSearch

core/src/main/java/org/elasticsearch/action/index/TransportIndexAction.java
@@ -39,11 +39,11 @@ import org.elasticsearch.cluster.service.ClusterService;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.engine.Engine;
+import org.elasticsearch.index.mapper.MapperParsingException;
 import org.elasticsearch.index.mapper.Mapping;
 import org.elasticsearch.index.mapper.SourceToParse;
 import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.index.shard.ShardId;
-import org.elasticsearch.index.translog.Translog.Location;
 import org.elasticsearch.indices.IndexAlreadyExistsException;
 import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.tasks.Task;
@@ -140,48 +140,68 @@ public class TransportIndexAction extends TransportWriteAction<IndexRequest, Ind
     }
 
     @Override
-    protected WriteResult<IndexResponse> onPrimaryShard(IndexRequest request, IndexShard indexShard) throws Exception {
-        return executeIndexRequestOnPrimary(request, indexShard, mappingUpdatedAction);
+    protected PrimaryOperationResult<IndexResponse> onPrimaryShard(IndexRequest request, IndexShard primary) throws Exception {
+        return executeIndexRequestOnPrimary(request, primary, mappingUpdatedAction);
     }
 
     @Override
-    protected Location onReplicaShard(IndexRequest request, IndexShard indexShard) {
-        return executeIndexRequestOnReplica(request, indexShard).getTranslogLocation();
+    protected ReplicaOperationResult onReplicaShard(IndexRequest request, IndexShard replica) {
+        return executeIndexRequestOnReplica(request, replica);
     }
 
     /**
      * Execute the given {@link IndexRequest} on a replica shard, throwing a
      * {@link RetryOnReplicaException} if the operation needs to be re-tried.
      */
-    public static Engine.Index executeIndexRequestOnReplica(IndexRequest request, IndexShard indexShard) {
+    public static ReplicaOperationResult executeIndexRequestOnReplica(IndexRequest request, IndexShard indexShard) {
         final ShardId shardId = indexShard.shardId();
         SourceToParse sourceToParse = SourceToParse.source(SourceToParse.Origin.REPLICA, shardId.getIndexName(), request.type(), request.id(), request.source())
                 .routing(request.routing()).parent(request.parent()).timestamp(request.timestamp()).ttl(request.ttl());
 
-        final Engine.Index operation = indexShard.prepareIndexOnReplica(sourceToParse, request.version(), request.versionType(), request.getAutoGeneratedTimestamp(), request.isRetry());
+        final Engine.Index operation;
+        try {
+            operation = indexShard.prepareIndexOnReplica(sourceToParse, request.version(), request.versionType(), request.getAutoGeneratedTimestamp(), request.isRetry());
+        } catch (MapperParsingException | IllegalArgumentException e) {
+            return new ReplicaOperationResult(e);
+        }
         Mapping update = operation.parsedDoc().dynamicMappingsUpdate();
         if (update != null) {
             throw new RetryOnReplicaException(shardId, "Mappings are not available on the replica yet, triggered update: " + update);
         }
         indexShard.index(operation);
-        return operation;
+        if (operation.hasFailure()) {
+            return new ReplicaOperationResult(operation.getFailure());
+        } else {
+            return new ReplicaOperationResult(operation.getTranslogLocation());
+        }
     }
 
     /** Utility method to prepare an index operation on primary shards */
-    public static Engine.Index prepareIndexOperationOnPrimary(IndexRequest request, IndexShard indexShard) {
+    static Engine.Index prepareIndexOperationOnPrimary(IndexRequest request, IndexShard indexShard) {
         SourceToParse sourceToParse = SourceToParse.source(SourceToParse.Origin.PRIMARY, request.index(), request.type(), request.id(), request.source())
             .routing(request.routing()).parent(request.parent()).timestamp(request.timestamp()).ttl(request.ttl());
         return indexShard.prepareIndexOnPrimary(sourceToParse, request.version(), request.versionType(), request.getAutoGeneratedTimestamp(), request.isRetry());
     }
 
-    public static WriteResult<IndexResponse> executeIndexRequestOnPrimary(IndexRequest request, IndexShard indexShard,
+    public static PrimaryOperationResult<IndexResponse> executeIndexRequestOnPrimary(IndexRequest request, IndexShard indexShard,
             MappingUpdatedAction mappingUpdatedAction) throws Exception {
-        Engine.Index operation = prepareIndexOperationOnPrimary(request, indexShard);
+        Engine.Index operation;
+        try {
+            operation = prepareIndexOperationOnPrimary(request, indexShard);
+        } catch (MapperParsingException | IllegalArgumentException e) {
+            return new PrimaryOperationResult<>(e);
+        }
         Mapping update = operation.parsedDoc().dynamicMappingsUpdate();
         final ShardId shardId = indexShard.shardId();
         if (update != null) {
-            mappingUpdatedAction.updateMappingOnMaster(shardId.getIndex(), request.type(), update);
-            operation = prepareIndexOperationOnPrimary(request, indexShard);
+            try {
+                // can throw timeout exception when updating mappings or ISE for attempting to update default mappings
+                // which are bubbled up
+                mappingUpdatedAction.updateMappingOnMaster(shardId.getIndex(), request.type(), update);
+                operation = prepareIndexOperationOnPrimary(request, indexShard);
+            } catch (MapperParsingException | IllegalArgumentException e) {
+                return new PrimaryOperationResult<>(e);
+            }
             update = operation.parsedDoc().dynamicMappingsUpdate();
             if (update != null) {
                 throw new ReplicationOperation.RetryOnPrimaryException(shardId,
@@ -189,16 +209,19 @@ public class TransportIndexAction extends TransportWriteAction<IndexRequest, Ind
             }
         }
         indexShard.index(operation);
+        if (operation.hasFailure()) {
+            return new PrimaryOperationResult<>(operation.getFailure());
+        } else {
+            // update the version on request so it will happen on the replicas
+            final long version = operation.version();
+            request.version(version);
+            request.versionType(request.versionType().versionTypeForReplicationAndRecovery());
 
-        // update the version on request so it will happen on the replicas
-        final long version = operation.version();
-        request.version(version);
-        request.versionType(request.versionType().versionTypeForReplicationAndRecovery());
+            assert request.versionType().validateVersionForWrites(request.version());
 
-        assert request.versionType().validateVersionForWrites(request.version());
-
-        IndexResponse response = new IndexResponse(shardId, request.type(), request.id(), request.version(), operation.isCreated());
-        return new WriteResult<>(response, operation.getTranslogLocation());
+            IndexResponse response = new IndexResponse(shardId, request.type(), request.id(), request.version(), operation.isCreated());
+            return new PrimaryOperationResult<>(response, operation.getTranslogLocation());
+        }
     }
 }
 

core/src/test/java/org/elasticsearch/action/support/replication/TransportWriteActionTests.java
@@ -28,7 +28,6 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.shard.IndexShard;
 import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.index.translog.Translog;
-import org.elasticsearch.index.translog.Translog.Location;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.threadpool.ThreadPool;
 import org.elasticsearch.transport.TransportService;
@@ -137,13 +136,13 @@ public class TransportWriteActionTests extends ESTestCase {
         }
 
         @Override
-        protected WriteResult<TestResponse> onPrimaryShard(TestRequest request, IndexShard indexShard) throws Exception {
-            return new WriteResult<>(new TestResponse(), location);
+        protected PrimaryOperationResult<TestResponse> onPrimaryShard(TestRequest request, IndexShard primary) throws Exception {
+            return new PrimaryOperationResult<>(new TestResponse(), location);
         }
 
         @Override
-        protected Location onReplicaShard(TestRequest request, IndexShard indexShard) {
-            return location;
+        protected ReplicaOperationResult onReplicaShard(TestRequest request, IndexShard replica) {
+            return new ReplicaOperationResult(location);
         }
 
         @Override