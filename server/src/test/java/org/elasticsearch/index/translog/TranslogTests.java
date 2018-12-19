ochprince/OpenSearch

commit b23d112abfd8e4549f0fc2d79f16c3f6e209de44

Remove legacy mapping code. (#29224)
Some features have been deprecated since `6.0` like the `_parent` field or the
ability to have multiple types per index. This allows to remove quite some
code, which in-turn will hopefully make it easier to proceed with the removal
of types.

server/src/main/java/org/elasticsearch/index/translog/Translog.java
@@ -23,7 +23,6 @@ import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.store.AlreadyClosedException;
 import org.elasticsearch.Version;
-import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.common.UUIDs;
 import org.elasticsearch.common.bytes.BytesArray;
 import org.elasticsearch.common.bytes.BytesReference;
@@ -41,7 +40,6 @@ import org.elasticsearch.core.internal.io.IOUtils;
 import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.VersionType;
 import org.elasticsearch.index.engine.Engine;
-import org.elasticsearch.index.mapper.Uid;
 import org.elasticsearch.index.seqno.SequenceNumbers;
 import org.elasticsearch.index.shard.AbstractIndexShardComponent;
 import org.elasticsearch.index.shard.IndexShardComponent;
@@ -939,22 +937,19 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
 
         public final BytesReference source;
         public final String routing;
-        public final String parent;
 
-        public Source(BytesReference source, String routing, String parent) {
+        public Source(BytesReference source, String routing) {
             this.source = source;
             this.routing = routing;
-            this.parent = parent;
         }
 
     }
 
     public static class Index implements Operation {
 
-        public static final int FORMAT_2_X = 6; // since 2.0-beta1 and 1.1
-        public static final int FORMAT_AUTO_GENERATED_IDS = FORMAT_2_X + 1; // since 5.0.0-beta1
-        public static final int FORMAT_SEQ_NO = FORMAT_AUTO_GENERATED_IDS + 1; // since 6.0.0
-        public static final int SERIALIZATION_FORMAT = FORMAT_SEQ_NO;
+        public static final int FORMAT_6_0 = 8; // since 6.0.0
+        public static final int FORMAT_NO_PARENT = FORMAT_6_0 + 1; // since 7.0
+        public static final int SERIALIZATION_FORMAT = FORMAT_NO_PARENT;
 
         private final String id;
         private final long autoGeneratedIdTimestamp;
@@ -965,35 +960,23 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
         private final VersionType versionType;
         private final BytesReference source;
         private final String routing;
-        private final String parent;
 
         private Index(final StreamInput in) throws IOException {
             final int format = in.readVInt(); // SERIALIZATION_FORMAT
-            assert format >= FORMAT_2_X : "format was: " + format;
+            assert format >= FORMAT_6_0 : "format was: " + format;
             id = in.readString();
             type = in.readString();
             source = in.readBytesReference();
             routing = in.readOptionalString();
-            parent = in.readOptionalString();
-            this.version = in.readLong();
-            if (format < FORMAT_SEQ_NO) {
-                in.readLong(); // timestamp
-                in.readLong(); // ttl
+            if (format < FORMAT_NO_PARENT) {
+                in.readOptionalString(); // _parent
             }
+            this.version = in.readLong();
             this.versionType = VersionType.fromValue(in.readByte());
             assert versionType.validateVersionForWrites(this.version) : "invalid version for writes: " + this.version;
-            if (format >= FORMAT_AUTO_GENERATED_IDS) {
-                this.autoGeneratedIdTimestamp = in.readLong();
-            } else {
-                this.autoGeneratedIdTimestamp = IndexRequest.UNSET_AUTO_GENERATED_TIMESTAMP;
-            }
-            if (format >= FORMAT_SEQ_NO) {
-                seqNo = in.readLong();
-                primaryTerm = in.readLong();
-            } else {
-                seqNo = SequenceNumbers.UNASSIGNED_SEQ_NO;
-                primaryTerm = 0;
-            }
+            this.autoGeneratedIdTimestamp = in.readLong();
+            seqNo = in.readLong();
+            primaryTerm = in.readLong();
         }
 
         public Index(Engine.Index index, Engine.IndexResult indexResult) {
@@ -1001,7 +984,6 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
             this.type = index.type();
             this.source = index.source();
             this.routing = index.routing();
-            this.parent = index.parent();
             this.seqNo = indexResult.getSeqNo();
             this.primaryTerm = index.primaryTerm();
             this.version = indexResult.getVersion();
@@ -1010,11 +992,11 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
         }
 
         public Index(String type, String id, long seqNo, byte[] source) {
-            this(type, id, seqNo, Versions.MATCH_ANY, VersionType.INTERNAL, source, null, null, -1);
+            this(type, id, seqNo, Versions.MATCH_ANY, VersionType.INTERNAL, source, null, -1);
         }
 
         public Index(String type, String id, long seqNo, long version, VersionType versionType, byte[] source, String routing,
-                     String parent, long autoGeneratedIdTimestamp) {
+                     long autoGeneratedIdTimestamp) {
             this.type = type;
             this.id = id;
             this.source = new BytesArray(source);
@@ -1023,7 +1005,6 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
             this.version = version;
             this.versionType = versionType;
             this.routing = routing;
-            this.parent = parent;
             this.autoGeneratedIdTimestamp = autoGeneratedIdTimestamp;
         }
 
@@ -1047,10 +1028,6 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
 
         public String routing() {
             return this.routing;
-        }
-
-        public String parent() {
-            return this.parent;
         }
 
         public BytesReference source() {
@@ -1077,7 +1054,7 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
 
         @Override
         public Source getSource() {
-            return new Source(source, routing, parent);
+            return new Source(source, routing);
         }
 
         private void write(final StreamOutput out) throws IOException {
@@ -1086,7 +1063,6 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
             out.writeString(type);
             out.writeBytesReference(source);
             out.writeOptionalString(routing);
-            out.writeOptionalString(parent);
             out.writeLong(version);
 
             out.writeByte(versionType.getValue());
@@ -1119,7 +1095,7 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
             if (routing != null ? !routing.equals(index.routing) : index.routing != null) {
                 return false;
             }
-            return !(parent != null ? !parent.equals(index.parent) : index.parent != null);
+            return true;
 
         }
 
@@ -1133,7 +1109,6 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
             result = 31 * result + versionType.hashCode();
             result = 31 * result + source.hashCode();
             result = 31 * result + (routing != null ? routing.hashCode() : 0);
-            result = 31 * result + (parent != null ? parent.hashCode() : 0);
             result = 31 * result + Long.hashCode(autoGeneratedIdTimestamp);
             return result;
         }
@@ -1156,10 +1131,8 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
 
     public static class Delete implements Operation {
 
-        public static final int FORMAT_5_0 = 2; // 5.0 - 5.5
-        private static final int FORMAT_SINGLE_TYPE = FORMAT_5_0 + 1; // 5.5 - 6.0
-        private static final int FORMAT_SEQ_NO = FORMAT_SINGLE_TYPE + 1; // 6.0 - *
-        public static final int SERIALIZATION_FORMAT = FORMAT_SEQ_NO;
+        private static final int FORMAT_6_0 = 4; // 6.0 - *
+        public static final int SERIALIZATION_FORMAT = FORMAT_6_0;
 
         private final String type, id;
         private final Term uid;
@@ -1170,33 +1143,15 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
 
         private Delete(final StreamInput in) throws IOException {
             final int format = in.readVInt();// SERIALIZATION_FORMAT
-            assert format >= FORMAT_5_0 : "format was: " + format;
-            if (format >= FORMAT_SINGLE_TYPE) {
-                type = in.readString();
-                id = in.readString();
-                if (format >= FORMAT_SEQ_NO) {
-                    uid = new Term(in.readString(), in.readBytesRef());
-                } else {
-                    uid = new Term(in.readString(), in.readString());
-                }
-            } else {
-                uid = new Term(in.readString(), in.readString());
-                // the uid was constructed from the type and id so we can
-                // extract them back
-                Uid uidObject = Uid.createUid(uid.text());
-                type = uidObject.type();
-                id = uidObject.id();
-            }
+            assert format >= FORMAT_6_0 : "format was: " + format;
+            type = in.readString();
+            id = in.readString();
+            uid = new Term(in.readString(), in.readBytesRef());
             this.version = in.readLong();
             this.versionType = VersionType.fromValue(in.readByte());
             assert versionType.validateVersionForWrites(this.version);
-            if (format >= FORMAT_SEQ_NO) {
-                seqNo = in.readLong();
-                primaryTerm = in.readLong();
-            } else {
-                seqNo = SequenceNumbers.UNASSIGNED_SEQ_NO;
-                primaryTerm = 0;
-            }
+            seqNo = in.readLong();
+            primaryTerm = in.readLong();
         }
 
         public Delete(Engine.Delete delete, Engine.DeleteResult deleteResult) {

server/src/test/java/org/elasticsearch/index/translog/TranslogTests.java
@@ -20,6 +20,7 @@
 package org.elasticsearch.index.translog;
 
 import com.carrotsearch.randomizedtesting.generators.RandomPicks;
+
 import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.apache.lucene.codecs.CodecUtil;
 import org.apache.lucene.document.Field;
@@ -30,7 +31,6 @@ import org.apache.lucene.mockfile.FilterFileChannel;
 import org.apache.lucene.store.AlreadyClosedException;
 import org.apache.lucene.store.ByteArrayDataOutput;
 import org.apache.lucene.store.MockDirectoryWrapper;
-import org.elasticsearch.core.internal.io.IOUtils;
 import org.apache.lucene.util.LineFileDocs;
 import org.apache.lucene.util.LuceneTestCase;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
@@ -53,15 +53,16 @@ import org.elasticsearch.common.xcontent.ToXContent;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.core.internal.io.IOUtils;
 import org.elasticsearch.index.IndexSettings;
 import org.elasticsearch.index.VersionType;
 import org.elasticsearch.index.engine.Engine;
 import org.elasticsearch.index.engine.Engine.Operation.Origin;
+import org.elasticsearch.index.mapper.IdFieldMapper;
 import org.elasticsearch.index.mapper.ParseContext.Document;
 import org.elasticsearch.index.mapper.ParsedDocument;
 import org.elasticsearch.index.mapper.SeqNoFieldMapper;
 import org.elasticsearch.index.mapper.Uid;
-import org.elasticsearch.index.mapper.UidFieldMapper;
 import org.elasticsearch.index.seqno.LocalCheckpointTracker;
 import org.elasticsearch.index.seqno.LocalCheckpointTrackerTests;
 import org.elasticsearch.index.seqno.SequenceNumbers;
@@ -406,9 +407,9 @@ public class TranslogTests extends ESTestCase {
         {
             final TranslogStats stats = stats();
             assertThat(stats.estimatedNumberOfOperations(), equalTo(1));
-            assertThat(stats.getTranslogSizeInBytes(), equalTo(140L));
+            assertThat(stats.getTranslogSizeInBytes(), equalTo(139L));
             assertThat(stats.getUncommittedOperations(), equalTo(1));
-            assertThat(stats.getUncommittedSizeInBytes(), equalTo(140L));
+            assertThat(stats.getUncommittedSizeInBytes(), equalTo(139L));
             assertThat(stats.getEarliestLastModifiedAge(), greaterThan(1L));
         }
 
@@ -416,9 +417,9 @@ public class TranslogTests extends ESTestCase {
         {
             final TranslogStats stats = stats();
             assertThat(stats.estimatedNumberOfOperations(), equalTo(2));
-            assertThat(stats.getTranslogSizeInBytes(), equalTo(189L));
+            assertThat(stats.getTranslogSizeInBytes(), equalTo(188L));
             assertThat(stats.getUncommittedOperations(), equalTo(2));
-            assertThat(stats.getUncommittedSizeInBytes(), equalTo(189L));
+            assertThat(stats.getUncommittedSizeInBytes(), equalTo(188L));
             assertThat(stats.getEarliestLastModifiedAge(), greaterThan(1L));
         }
 
@@ -426,9 +427,9 @@ public class TranslogTests extends ESTestCase {
         {
             final TranslogStats stats = stats();
             assertThat(stats.estimatedNumberOfOperations(), equalTo(3));
-            assertThat(stats.getTranslogSizeInBytes(), equalTo(238L));
+            assertThat(stats.getTranslogSizeInBytes(), equalTo(237L));
             assertThat(stats.getUncommittedOperations(), equalTo(3));
-            assertThat(stats.getUncommittedSizeInBytes(), equalTo(238L));
+            assertThat(stats.getUncommittedSizeInBytes(), equalTo(237L));
             assertThat(stats.getEarliestLastModifiedAge(), greaterThan(1L));
         }
 
@@ -436,13 +437,13 @@ public class TranslogTests extends ESTestCase {
         {
             final TranslogStats stats = stats();
             assertThat(stats.estimatedNumberOfOperations(), equalTo(4));
-            assertThat(stats.getTranslogSizeInBytes(), equalTo(280L));
+            assertThat(stats.getTranslogSizeInBytes(), equalTo(279L));
             assertThat(stats.getUncommittedOperations(), equalTo(4));
-            assertThat(stats.getUncommittedSizeInBytes(), equalTo(280L));
+            assertThat(stats.getUncommittedSizeInBytes(), equalTo(279L));
             assertThat(stats.getEarliestLastModifiedAge(), greaterThan(1L));
         }
 
-        final long expectedSizeInBytes = 323L;
+        final long expectedSizeInBytes = 322L;
         translog.rollGeneration();
         {
             final TranslogStats stats = stats();
@@ -828,11 +829,11 @@ public class TranslogTests extends ESTestCase {
     }
 
     private Term newUid(ParsedDocument doc) {
-        return new Term("_uid", Uid.createUidAsBytes(doc.type(), doc.id()));
+        return new Term("_id", Uid.encodeId(doc.id()));
     }
 
-    private Term newUid(String uid) {
-        return new Term("_uid", uid);
+    private Term newUid(String id) {
+        return new Term("_id", Uid.encodeId(id));
     }
 
     public void testVerifyTranslogIsNotDeleted() throws IOException {
@@ -1468,7 +1469,7 @@ public class TranslogTests extends ESTestCase {
         try (Translog ignored = new Translog(config, translogUUID, deletionPolicy, () -> SequenceNumbers.NO_OPS_PERFORMED)) {
             fail("corrupted");
         } catch (IllegalStateException ex) {
-            assertEquals("Checkpoint file translog-3.ckp already exists but has corrupted content expected: Checkpoint{offset=3123, " +
+            assertEquals("Checkpoint file translog-3.ckp already exists but has corrupted content expected: Checkpoint{offset=3068, " +
                 "numOps=55, generation=3, minSeqNo=45, maxSeqNo=99, globalCheckpoint=-1, minTranslogGeneration=1} but got: Checkpoint{offset=0, numOps=0, " +
                 "generation=0, minSeqNo=-1, maxSeqNo=-1, globalCheckpoint=-1, minTranslogGeneration=0}", ex.getMessage());
         }
@@ -2449,11 +2450,11 @@ public class TranslogTests extends ESTestCase {
         seqID.seqNo.setLongValue(randomSeqNum);
         seqID.seqNoDocValue.setLongValue(randomSeqNum);
         seqID.primaryTerm.setLongValue(randomPrimaryTerm);
-        Field uidField = new Field("_uid", Uid.createUid("test", "1"), UidFieldMapper.Defaults.FIELD_TYPE);
+        Field idField = new Field("_id", Uid.encodeId("1"), IdFieldMapper.Defaults.FIELD_TYPE);
         Field versionField = new NumericDocValuesField("_version", 1);
         Document document = new Document();
         document.add(new TextField("value", "test", Field.Store.YES));
-        document.add(uidField);
+        document.add(idField);
         document.add(versionField);
         document.add(seqID.seqNo);
         document.add(seqID.seqNoDocValue);
@@ -2482,21 +2483,6 @@ public class TranslogTests extends ESTestCase {
         in = out.bytes().streamInput();
         Translog.Delete serializedDelete = (Translog.Delete) Translog.Operation.readOperation(in);
         assertEquals(delete, serializedDelete);
-
-        // simulate legacy delete serialization
-        out = new BytesStreamOutput();
-        out.writeByte(Translog.Operation.Type.DELETE.id());
-        out.writeVInt(Translog.Delete.FORMAT_5_0);
-        out.writeString(UidFieldMapper.NAME);
-        out.writeString("my_type#my_id");
-        out.writeLong(3); // version
-        out.writeByte(VersionType.INTERNAL.getValue());
-        out.writeLong(2); // seq no
-        out.writeLong(0); // primary term
-        in = out.bytes().streamInput();
-        serializedDelete = (Translog.Delete) Translog.Operation.readOperation(in);
-        assertEquals("my_type", serializedDelete.type());
-        assertEquals("my_id", serializedDelete.id());
     }
 
     public void testRollGeneration() throws Exception {

commit 36de84e8b9474a0d7735ae55c22a40c5f864ed0b

Fix translog bwc serialization (#36676)
Serializing of a Translog#Index from v7.0.0 to 6.x is broken since
#29224 where we removed the _parent field.

Relates #29224

server/src/main/java/org/elasticsearch/index/translog/Translog.java
@@ -1227,6 +1227,9 @@ public class Translog extends AbstractIndexShardComponent implements IndexShardC
             out.writeString(type);
             out.writeBytesReference(source);
             out.writeOptionalString(routing);
+            if (format < FORMAT_NO_PARENT) {
+                 out.writeOptionalString(null); // _parent
+            }
             out.writeLong(version);
             if (format < FORMAT_NO_VERSION_TYPE) {
                 out.writeByte(VersionType.EXTERNAL.getValue());

server/src/test/java/org/elasticsearch/index/translog/TranslogTests.java
@@ -35,6 +35,7 @@ import org.apache.lucene.store.MockDirectoryWrapper;
 import org.apache.lucene.util.LineFileDocs;
 import org.apache.lucene.util.LuceneTestCase;
 import org.elasticsearch.Assertions;
+import org.elasticsearch.Version;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.common.Randomness;
 import org.elasticsearch.common.Strings;
@@ -72,6 +73,7 @@ import org.elasticsearch.index.shard.ShardId;
 import org.elasticsearch.index.translog.Translog.Location;
 import org.elasticsearch.test.ESTestCase;
 import org.elasticsearch.test.IndexSettingsModule;
+import org.elasticsearch.test.VersionUtils;
 import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
@@ -2814,9 +2816,12 @@ public class TranslogTests extends ESTestCase {
         Engine.IndexResult eIndexResult = new Engine.IndexResult(1, randomPrimaryTerm, randomSeqNum, true);
         Translog.Index index = new Translog.Index(eIndex, eIndexResult);
 
+        Version wireVersion = VersionUtils.randomVersionBetween(random(), Version.CURRENT.minimumCompatibilityVersion(), Version.CURRENT);
         BytesStreamOutput out = new BytesStreamOutput();
+        out.setVersion(wireVersion);
         Translog.Operation.writeOperation(out, index);
         StreamInput in = out.bytes().streamInput();
+        in.setVersion(wireVersion);
         Translog.Index serializedIndex = (Translog.Index) Translog.Operation.readOperation(in);
         assertEquals(index, serializedIndex);
 
@@ -2826,8 +2831,10 @@ public class TranslogTests extends ESTestCase {
         Translog.Delete delete = new Translog.Delete(eDelete, eDeleteResult);
 
         out = new BytesStreamOutput();
+        out.setVersion(wireVersion);
         Translog.Operation.writeOperation(out, delete);
         in = out.bytes().streamInput();
+        in.setVersion(wireVersion);
         Translog.Delete serializedDelete = (Translog.Delete) Translog.Operation.readOperation(in);
         assertEquals(delete, serializedDelete);
     }