elastic/elasticsearch

Validate read priv of enrich source indices (#43595)
This commit adds permissions validation on the indices provided in the
enrich policy. These indices should be validated at store time so as not
to have cryptic error messages in the event the user does not have
permissions to access said indices.

x-pack/plugin/enrich/src/test/java/org/elasticsearch/xpack/enrich/EnrichPolicyUpdateTests.java
@@ -27,7 +27,7 @@ public class EnrichPolicyUpdateTests extends ESSingleNodeTestCase {
 
     @Override
     protected Collection<Class<? extends Plugin>> getPlugins() {
-        return List.of(EnrichPlugin.class);
+        return List.of(LocalStateEnrich.class);
     }
 
     public void testUpdatePolicyOnly() {
@@ -56,6 +56,4 @@ public class EnrichPolicyUpdateTests extends ESSingleNodeTestCase {
         Pipeline pipelineInstance2 = ingestService.getPipeline("1");
         assertThat(pipelineInstance2, sameInstance(pipelineInstance1));
     }
-
-
 }

ochprince/OpenSearch

x-pack/plugin/enrich/src/test/java/org/elasticsearch/xpack/enrich/EnrichPolicyUpdateTests.java
@@ -27,7 +27,7 @@ public class EnrichPolicyUpdateTests extends ESSingleNodeTestCase {
 
     @Override
     protected Collection<Class<? extends Plugin>> getPlugins() {
-        return Collections.singleton(EnrichPlugin.class);
+        return Collections.singleton(LocalStateEnrich.class);
     }
 
     public void testUpdatePolicyOnly() {
@@ -56,6 +56,4 @@ public class EnrichPolicyUpdateTests extends ESSingleNodeTestCase {
         Pipeline pipelineInstance2 = ingestService.getPipeline("1");
         assertThat(pipelineInstance2, sameInstance(pipelineInstance1));
     }
-
-
 }